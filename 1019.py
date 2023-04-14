23f8b3fd1b21514f6c78db197f436a52569d91ed
alissonjc <alisson.cavalcante94@gmail.com>
2023-04-14 01:31:32 -0300

    Removing comments sections

--- 1019.py
+++ 1019.py
@@ -1,13 +1,3 @@
-# Read an integer value, which is the duration in seconds of a certain event
-# in a factory, and inform it expressed in hours:minutes:seconds.
-#
-# Input
-# The input file contains an integer N.
-#
-# Output
-# Print the read time in the input file (seconds) converted in hours:minutes:seconds
-# like the following example.
-
 duration = int(input())
 
 hours = duration // 3600
