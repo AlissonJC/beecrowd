5fe56b2f9d29f244a797a015eb4a2c7aa9abccb4
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-04 16:41:03 -0300

    Problem 2750

--- 2750.py
+++ 2750.py
@@ -3,13 +3,13 @@ values = []
 for i in range(16):
     values.append(i)
 
-print("---------------------------------------")
+print("-"*39)
 print("|  decimal  |  octal  |  Hexadecimal  |")
-print("---------------------------------------")
+print("-"*39)
 for i in range(8):
     print(f"|      {values[i]}    |    {oct(values[i])[2:]}    |       {hex(values[i])[2:].upper()}       |")
 for i in range(8, 10):
     print(f"|      {values[i]}    |   {oct(values[i])[2:]}    |       {hex(values[i])[2:].upper()}       |")
 for i in range(10, 16):
     print(f"|     {values[i]}    |   {oct(values[i])[2:]}    |       {hex(values[i])[2:].upper()}       |")
-print("---------------------------------------")
+print("-"*39)
