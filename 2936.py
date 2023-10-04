e78ec3d521e388cc0b79221030b4b52dd28b51f5
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-04 01:36:27 -0300

    Problem 2936

---
+++ 2936.py
@@ -0,0 +1,16 @@
+curupira = int(input())
+boitata = int(input())
+boto = int(input())
+mapinguari = int(input())
+iara = int(input())
+dona_chica = 225
+
+curupira *= 300
+boitata *= 1500
+boto *= 600
+mapinguari *= 1000
+iara *= 150
+
+total = dona_chica + curupira + boitata + boto + mapinguari + iara
+
+print(total)
