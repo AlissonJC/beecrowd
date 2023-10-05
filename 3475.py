efcc60a05d4bc79bc4c832a512177a55258e82e6
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-05 18:51:55 -0300

    Problem 3475

---
+++ 3475.py
@@ -0,0 +1,42 @@
+word = input()
+
+if word == "1":
+    print("um")
+elif word == "um":
+    print("1")
+elif word == "2":
+    print("dois")
+elif word == "dois":
+    print("2")
+elif word == "3":
+    print("tres")
+elif word == "tres":
+    print("3")
+elif word == "4":
+    print("quatro")
+elif word == "quatro":
+    print("4")
+elif word == "5":
+    print("cinco")
+elif word == "cinco":
+    print("5")
+elif word == "6":
+    print("seis")
+elif word == "seis":
+    print("6")
+elif word == "7":
+    print("sete")
+elif word == "sete":
+    print("7")
+elif word == "8":
+    print("oito")
+elif word == "oito":
+    print("8")
+elif word == "9":
+    print("nove")
+elif word == "nove":
+    print("9")
+elif word == "0":
+    print("zero")
+elif word == "zero":
+    print("0")
