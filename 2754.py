8920ef0451b5c0cc3ae76c937eac7c7bdd8f1da2
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-16 14:25:49 -0300

    problem 2754

---
+++ 2754.py
@@ -0,0 +1,12 @@
+a = 234.345
+b = 45.698
+
+print(f"{a:.6f} - {b:.6f}")
+print(f"{a:.0f} - {b:.0f}")
+print(f"{a:.1f} - {b:.1f}")
+print(f"{a:.2f} - {b:.2f}")
+print(f"{a:.3f} - {b:.3f}")
+print(f"{a:.6e} - {b:.6e}")
+print(f"{a:.6E} - {b:.6E}")
+print(f"{a:.3f} - {b:.3f}")
+print(f"{a:.3f} - {b:.3f}")
