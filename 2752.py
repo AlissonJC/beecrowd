c71051f4a39d86d6af3921748757c7619ac94033
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-16 16:07:55 -0300

    problem 2752

---
+++ 2752.py
@@ -0,0 +1,10 @@
+word = "AMO FAZER EXERCICIO NO URI"
+
+print(f"<{word}>")
+print(f"<{' '*4}{word}>")
+print(f"<{word:.20s}>")
+print(f"<{word}>")
+print(f"<{word:30s}>")
+print(f"<{word:.30s}>")
+print(f"<{' '*10}{word:.20s}>")
+print(f"<{word:30.20s}>")
