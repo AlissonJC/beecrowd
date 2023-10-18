0ae7719b2c379d7df490b089402bf922fea29f55
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-18 14:02:31 -0300

    problem 1943

---
+++ 1943.py
@@ -0,0 +1,16 @@
+k = int(input())
+
+if k == 1:
+    print("Top 1")
+elif 2 <= k <= 3:
+    print("Top 3")
+elif 4 <= k <= 5:
+    print("Top 5")
+elif 6 <= k <= 10:
+    print("Top 10")
+elif 11 <= k <= 25:
+    print("Top 25")
+elif 26 <= k <= 50:
+    print("Top 50")
+elif 51 <= k <= 100:
+    print("Top 100")
