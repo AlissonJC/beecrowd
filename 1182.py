21cbbae8b17999c91b784e5f5a505db7ddbc4a78
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-30 13:20:32 -0300

    problem 1182

---
+++ 1182.py
@@ -0,0 +1,20 @@
+column = int(input())
+
+op = input()
+
+M = []
+for i in range(12):
+    line = []
+    for j in range(12):
+        line.append(float(input()))
+    M.append(line)
+
+total = 0.0
+
+for i in range(12):
+    total += M[i][column]
+
+if op == 'M':
+    total /= 12
+
+print(f'{total:.1f}')
