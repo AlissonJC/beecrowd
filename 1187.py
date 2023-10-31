0059a819245c855e2e93994b25eeba7eaf188e08
alissonjc <alisson.cavalcante94@gmail.com>
2023-10-30 21:38:23 -0300

    problem 1187

---
+++ 1187.py
@@ -0,0 +1,26 @@
+def main():
+    c = 12
+    m = [[0.0] * c for _ in range(c)]
+    soma = 0.0
+    media = 0.0
+    elementos = 0
+
+    o = input()
+
+    for i in range(c):
+        for j in range(c):
+            m[i][j] = float(input())
+
+    for i in range(5):
+        for j in range(i + 1, 11 - i):
+            soma += m[i][j]
+            elementos += 1
+
+    if o == 'S':
+        print(f'{soma:.1f}')
+    else:
+        media = soma / elementos
+        print(f'{media:.1f}')
+
+if __name__ == "__main__":
+    main()
